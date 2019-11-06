9d0e186acd3a7e5de639a559702e62fffb0de0dc
Calin Culianu <calin.culianu@gmail.com>
2019-11-06 17:01:12 +0200

    Follow-up to last: fixed a typo. Darn this keyboard. :)

--- electroncash/commands.py
+++ electroncash/commands.py
@@ -609,7 +609,7 @@ class Commands(PrintError):
                 assert tmp == op_return_raw.lower()
                 op_return_raw = tmp
             except Exception as e:
-                raise RuntimeError("op_return_raw must be an even number of be hex digits") from e
+                raise RuntimeError("op_return_raw must be an even number of hex digits") from e
             final_outputs.append(self._output_for_opreturn_rawhex(op_return_raw))
         for address, amount in outputs:
             address = self._resolver(address)
