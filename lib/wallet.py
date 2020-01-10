d774233a56d716c99d88b31592b283ebe206b192
Axel Gembe <derago@gmail.com>
2020-01-10 19:50:24 +0700

    Wallet.py: Fix warning about "is" with a literal
    lib/wallet.py:1556: SyntaxWarning: "is" with a literal. Did you mean "=="?
      if label is '':

--- lib/wallet.py
+++ lib/wallet.py
@@ -2024,7 +2024,7 @@ class Abstract_Wallet(PrintError):
 
     def get_label(self, tx_hash):
         label = self.labels.get(tx_hash, '')
-        if label is '':
+        if not label:
             label = self.get_default_label(tx_hash)
         return label
 
