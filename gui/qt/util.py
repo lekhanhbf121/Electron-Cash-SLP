90ffc3319eec002b44927a89cc7d0c230e8a0ae5
Calin Culianu <calin.culianu@gmail.com>
2020-01-26 23:23:43 +0200

    Backport from CashFusion branch: util.ColorSheme.get_html()
    This returns the '#ff3399' or whatever html string for a particular
    color scheme item.

--- gui/qt/util.py
+++ gui/qt/util.py
@@ -986,6 +986,9 @@ class ColorSchemeItem:
         color = self._get_color(background)
         return QColor(color)
 
+    def get_html(self, background=False):
+        return self._get_color(background)
+
 
 class _ColorScheme:
     def __init__(self):
