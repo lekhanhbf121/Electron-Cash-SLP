spesmilo/electrum

trezor: bump lib version, implement new passphrase-on-device UI

electrum/plugins/trezor/cmdline.py
@@ -1,6 +1,21 @@
 from electrum.plugin import hook
-from .trezor import TrezorPlugin
+from electrum.i18n import _
+from electrum.util import print_stderr
+from .trezor import TrezorPlugin, PASSPHRASE_ON_DEVICE
 from ..hw_wallet import CmdLineHandler
+
+class TrezorCmdLineHandler(CmdLineHandler):
+    def __init__(self):
+        self.passphrase_on_device = False
+        super().__init__()
+
+    def get_passphrase(self, msg, confirm):
+        import getpass
+        print_stderr(msg)
+        if self.passphrase_on_device and self.yes_no_question(_('Enter passphrase on device?')):
+            return PASSPHRASE_ON_DEVICE
+        else:
+            return getpass.getpass('')
 
 class Plugin(TrezorPlugin):
     handler = CmdLineHandler()

lekhanhbf121/Electron-Cash-SLP

plugins/trezor/cmdline.py
@@ -1,6 +1,21 @@
 from electroncash.plugins import hook
-from .trezor import TrezorPlugin
+from electroncash.i18n import _
+from electroncash.util import print_stderr
+from .trezor import TrezorPlugin, PASSPHRASE_ON_DEVICE
 from ..hw_wallet import CmdLineHandler
+
+class TrezorCmdLineHandler(CmdLineHandler):
+    def __init__(self):
+        self.passphrase_on_device = False
+        super().__init__()
+
+    def get_passphrase(self, msg, confirm):
+        import getpass
+        print_stderr(msg)
+        if self.passphrase_on_device and self.yes_no_question(_('Enter passphrase on device?')):
+            return PASSPHRASE_ON_DEVICE
+        else:
+            return getpass.getpass('')
 
 class Plugin(TrezorPlugin):
     handler = CmdLineHandler()